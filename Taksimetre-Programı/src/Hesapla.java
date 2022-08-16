dd0af8d17d88a0e3edabb635fda80867329075ef
OzgurCetin72 <103455253+OzgurCetin72@users.noreply.github.com>
2022-08-16 23:52:12 +0300

    Update Hesapla.java

--- Taksimetre-Programı/src/Hesapla.java
+++ Taksimetre-Programı/src/Hesapla.java
@@ -6,8 +6,8 @@ public class Hesapla {
 		// TODO Auto-generated method stub
 		
 		int km;
-		double kmBasýna= 2.20;
-		int taksimetreAcýlýs=10;
+		double kmBasina= 2.20;
+		int taksimetreAcilis=10;
 		int minUcret=20;
 		double tutar;
 
@@ -17,7 +17,7 @@ public class Hesapla {
 		System.out.println("Km Mesafesini Giriniz : ");
 		km=input.nextInt();
 		
-		tutar= (km * kmBasýna) + taksimetreAcýlýs; 
+		tutar= (km * kmBasina) + taksimetreAcilis; 
 		
 		System.out.println( (tutar>20) ? "TUTAR : " + tutar + " TL" : "TUTAR : " + minUcret + " TL" );
 		
