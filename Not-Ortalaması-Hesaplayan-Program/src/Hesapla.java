65800e9735a06962df4f580b94aa33959496e161
OzgurCetin72 <103455253+OzgurCetin72@users.noreply.github.com>
2022-08-16 21:57:07 +0300

    Add files via upload

---
+++ Not-Ortalaması-Hesaplayan-Program/src/Hesapla.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class Hesapla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int matematik , fizik , kimya , turkce, tarih , muzik; 
+		
+		System.out.println(" matematik notunuzu giriniz : ");
+		matematik =input.nextInt();
+		
+		System.out.println(" fizik notunuzu giriniz : ");
+		fizik =input.nextInt();
+		
+		System.out.println(" kimya notunuzu giriniz : ");
+		kimya =input.nextInt();
+		
+		System.out.println(" turkce notunuzu giriniz : ");
+		turkce =input.nextInt();
+		
+		System.out.println(" tarih notunuzu giriniz : ");
+		tarih =input.nextInt();
+		
+		System.out.println(" muzik notunuzu giriniz : ");
+		muzik =input.nextInt();
+		
+		
+		int toplam = (matematik + matematik + kimya + turkce + tarih + muzik );
+		
+		double sonuc = toplam / 6 ;
+		
+		
+		
+		System.out.println((sonuc>60) ? "Ortalamýnýz " + sonuc + " Sýnýfý Geçtiniz..": "Ortalamýnýz " + sonuc + " Sýnýfta Kaldýnýz..");
+		
+		
+		
+		
+		
+
+	}
+
+}
