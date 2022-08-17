14a3eef5a588935545ed56a2b41203500166fd33
OzgurCetin72 <103455253+OzgurCetin72@users.noreply.github.com>
2022-08-18 02:23:40 +0300

    Add files via upload

---
+++ Artık-Yil-Hesaplama/src/Hesapla.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Hesapla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int yil;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Yýl Giriniz : ");
+		yil = input.nextInt();
+
+		// oncelikle 100'un kati olup olmadigini kontrol ediyoruz.
+		double hesap1 = yil % 100;
+
+		if (hesap1 == 0) {
+
+			// 100'un kati ise 400'e tam bolunup bolunmedigine bakiyoruz.
+			double hesap2 = yil % 400;
+
+			if (hesap2 == 0) {
+
+				System.out.println(yil + " bir artik yildir ! ");
+			} else {
+
+				System.out.println(yil + " bir artik yil degildir ! ");
+			}
+
+		} else {
+
+			// 100'un kati degil ise 4'e tam bolunup bolunmedigine bakýyoruz.
+			double hesap3 = yil % 4;
+
+			if (hesap3 == 0) {
+
+				System.out.println(yil + " bir artýk yildir ! ");
+			} else {
+
+				System.out.println(yil + " bir artýk yil degildir ! ");
+			}
+		}
+
+	}
+
+}
