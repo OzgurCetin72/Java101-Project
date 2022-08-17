092dd5e5e8e6d85d1906d8d1dffdd571de0f08e5
OzgurCetin72 <103455253+OzgurCetin72@users.noreply.github.com>
2022-08-18 01:20:15 +0300

    Update Hesapla.java

--- Ucak-Bileti-Fiyati-Hesaplama/src/Hesapla.java
+++ Ucak-Bileti-Fiyati-Hesaplama/src/Hesapla.java
@@ -6,7 +6,7 @@ public class Hesapla {
 		// TODO Auto-generated method stub
 
 		int mesafe, yas, tip;
-		double yasİndirimOranı;
+		double yasÄ°ndirimOranÄ±;
 		
 
 		Scanner input = new Scanner(System.in);
@@ -18,34 +18,34 @@ public class Hesapla {
 		yas = input.nextInt();
 
 		System.out.println("Yolculuk tipinizi giriniz : ");
-		System.out.println("1 => Tek Yön\n2 => Gidis-Donus");
+		System.out.println("1 => Tek YÃ¶n\n2 => Gidis-Donus");
 		tip = input.nextInt();
 
 		if ((mesafe > 0) && (yas > 0) && (tip == 1 || tip == 2)) {
 
 			if (yas < 12) {
-				yasİndirimOranı = 0.50;
+				yasÄ°ndirimOranÄ± = 0.50;
 
 			} else if (yas > 12 && yas < 24) {
-				yasİndirimOranı = 0.10;
+				yasÄ°ndirimOranÄ± = 0.10;
 
 			} else if (yas > 65) {
-				yasİndirimOranı = 0.30;
+				yasÄ°ndirimOranÄ± = 0.30;
 
 			} else {
-				yasİndirimOranı = 0;
+				yasÄ°ndirimOranÄ± = 0;
 			}
 			
 			
 			double	normalTutar = mesafe * 0.10 ;
-			double	yasİndirimi = normalTutar * yasİndirimOranı ;
-			double	indirimliTutar = normalTutar - yasİndirimi ;
+			double	yasÄ°ndirimi = normalTutar * yasÄ°ndirimOranÄ± ;
+			double	indirimliTutar = normalTutar - yasÄ°ndirimi ;
 			double	toplamTutar;
 			
 			if (tip==2) {
 				
-				double	gidisDonusİndirimi = indirimliTutar * 0.20 ;
-				toplamTutar = (indirimliTutar - gidisDonusİndirimi) * 2 ;
+				double	gidisDonusÄ°ndirimi = indirimliTutar * 0.20 ;
+				toplamTutar = (indirimliTutar - gidisDonusÄ°ndirimi) * 2 ;
 			
 			}else {
 				
@@ -55,18 +55,11 @@ public class Hesapla {
 			
 			System.out.println("Toplam Tutar : " + toplamTutar + " TL");
 			
-			
-			
-			
-			
-			
-			
-			
-			
+	
 
 		} else {
 
-			System.out.println(" Hatalı Veri Girdiniz.. ! ");
+			System.out.println(" HatalÄ± Veri Girdiniz.. ! ");
 
 		}
 
