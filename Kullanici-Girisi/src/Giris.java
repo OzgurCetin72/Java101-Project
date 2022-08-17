12d06365f69c50ae54d0432822cd4f2b852c421a
OzgurCetin72 <103455253+OzgurCetin72@users.noreply.github.com>
2022-08-17 13:40:12 +0300

    Add files via upload

---
+++ Kullanici-Girisi/src/Giris.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Giris {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String userName,password;
+		
+		Scanner input =new Scanner(System.in);
+		
+		System.out.println("Kullanici Adinizi Giriniz : ");
+		userName=input.nextLine();
+		
+		System.out.println("Sifrenizi Giriniz : ");
+		password=input.nextLine();
+		
+		if ((userName.equals("patika")) && (password.equals("java123"))) {
+			
+			System.out.println(" Giris Yaptýnýz.. ");
+			
+		}else {
+			
+			System.out.println(" Bilgileriniz Yanlis.. ");
+			
+			System.out.println("Sifrenizi degistirmek ister misiniz? (evet/hayýr) ");
+			String cevap=input.nextLine();
+			
+			if (cevap.equals("evet")) {
+				
+				System.out.println(" Yeni Sifrenizi Giriniz : ");
+				String newPassword=input.nextLine();
+				
+				if (!(newPassword.equals("java123"))) {
+					
+					  System.out.println(" Yeni sifreniz olusturuldu..");
+					
+				}else {
+					 
+					System.out.println("Sifre olusturulamadý, lutfen baska sifre giriniz..");
+				}
+				
+				
+			}else if (cevap.equals("hayýr")) {
+				
+				System.out.println(" iyi günler dileriz.. ");
+				
+			}else {
+				System.out.println(" Hatalý cevap.. ");
+			}
+			
+		}
+		
+		
+		
+
+	}
+
+}
